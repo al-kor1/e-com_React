2932ed8e2d850cc26d9ddffd3a0b0f4a32e70934
Oleksandr Korshunov <alkor1@protonmail.com>
2023-12-27 12:40:53 +0200

    Products.js was refactored and the repeated code was moved to the Card.js

--- e-com/src/Poducts.js/Products.js
+++ e-com/src/Poducts.js/Products.js
@@ -1,178 +1,18 @@
 import "./Products.css";
-import { AiFillStar } from "react-icons/ai";
-import { IoBagAdd } from "react-icons/io5";
+import Card from "../components/Card";
 
 export default function Products() {
   return <>
     <section className="card-container">
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
-      <section className="card">
-        <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <AiFillStar className="rating-star"/>
-            <span className="total-reviews">4</span>
-          </section>
-          <section className="card-price">
-            <div className="price">
-              <del>$70</del> <span>55</span>
-            </div>
-            <div className="bag">
-              <IoBagAdd className="bag-icon"/>
-            </div>
-          </section>
-        </div>
-      </section>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </section>
   </>
 }
---
+++ e-com/src/components/Card.js
@@ -0,0 +1,28 @@
+import { AiFillStar } from "react-icons/ai";
+import { IoBagAdd } from "react-icons/io5";
+
+export default function Card() {
+  return (
+    <section className="card">
+      <img className="card-img" src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
+      <div className="card-details">
+        <h3 className="card-title">Shoe</h3>
+        <section className="card-reviews">
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <span className="total-reviews">4</span>
+        </section>
+        <section className="card-price">
+          <div className="price">
+            <del>$70</del> <span>55</span>
+          </div>
+          <div className="bag">
+            <IoBagAdd className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+  )
+}
