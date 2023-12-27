0266aea6bef1cbb59f818be59af72387670470dd
Oleksandr Korshunov <alkor1@protonmail.com>
2023-12-27 11:08:08 +0200

    the Price.js styled

--- e-com/src/Sidebar/Price/Price.js
+++ e-com/src/Sidebar/Price/Price.js
@@ -2,6 +2,24 @@ import "./Price.css";
 
 export default function Price() {
   return (
-    <div>Price</div>
+    <div className="ml">
+      <h2 className="sidebar-title price-title"></h2>
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>$0-$50
+        </label>
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>$50-$100
+        </label>
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>$100-$150
+        </label>
+      <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Over $150
+        </label>
+    </div>
   )
 }
