2195b680a14d63f331cdc1fc038c55aefb555422
Oleksandr Korshunov <alkor1@protonmail.com>
2024-01-04 18:54:36 +0200

    initial filtring functions and variables declared

--- e-com/src/App.js
+++ e-com/src/App.js
@@ -3,19 +3,19 @@ import Nav from "./Navigation/Nav";
 import Products from "./Poducts.js/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
-
+import Card from "./components/Card";
 import products from "./db/data";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
 
   //Input Filter
-  const [query, setQuery] = useState("");
   const handleInputChange = event => {
     setQuery(event.target.value);
   };
 
-  const filtredItems = products.filter(product =>
+  const filteredItems = products.filter(product =>
     product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
   );
 
@@ -29,7 +29,41 @@ function App() {
     setSelectedCategory(event.target.value);
   }
 
-  
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    //Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    //Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ title, newPrice, company, color, category }) =>
+          category === selected ||
+          company === selected ||
+          color === selected ||
+          title === selected
+      );
+    }
+    return filteredProducts.map(({ img, title, star, reviews, prevPrice, newPrice }) => (
+      < Card key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ))
+
+
+  }
+
+
+
+
   return (
     <>
       <Sidebar />
